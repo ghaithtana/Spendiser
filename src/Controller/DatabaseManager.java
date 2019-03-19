57da7448bf7103e07b3a94f303165e10c27bbe27
Reistana <ghaith.tana@icloud.com>
2019-03-19 14:36:18 +0200

    Create DatabaseManager class

---
+++ src/Controller/DatabaseManager.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseManager {
+	public static final DatabaseManager instance = new DatabaseManager();
+	private Connection conn;
+
+	private DatabaseManager() {
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+			conn = DriverManager
+					.getConnection("jdbc:sqlserver://HP\\SQLEXPRESS:1435;user=sa;password=radwant12345;databaseName=Spendiser");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ResultSet select(String sql) throws SQLException {
+		Statement sta = conn.createStatement();
+		ResultSet rs = sta.executeQuery(sql);
+		return rs;
+	}
+}
