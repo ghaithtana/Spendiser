64f2323a6c35a0ef7a7d007a483faea72bf552a9
Reistana <ghaith.tana@icloud.com>
2019-04-23 13:25:06 +0300

    adding financial manager

---
+++ src/Controller/FinancialManager.java
@@ -0,0 +1,36 @@
+package Controller;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class FinancialManager {
+	public static final FinancialManager instance = new FinancialManager();
+
+	private FinancialManager() {
+	}
+
+	public double getBalance(int id) throws SQLException {
+		String sql = String.format("SELECT dbo.GETBALANCE(%d) As Balance ", id);
+		ResultSet rs = DatabaseManager.instance.select(sql);
+
+		if (rs.next()) {
+			return rs.getDouble("Balance");
+		}
+
+		return 0;
+	}
+
+	public double TotalOutcomes(int month) throws SQLException {
+
+		String sql = String.format("SELECT TotalOutcome FROM [Month] WHERE U_ID=%d AND [Month]=%d",
+				UserManager.instance.getCurrentUser().getU_ID(), month);
+		ResultSet rs = DatabaseManager.instance.select(sql);
+
+		if (rs.next()) {
+			return rs.getDouble("TotalOutcome");
+		}
+
+		return 0;
+	}
+
+}
