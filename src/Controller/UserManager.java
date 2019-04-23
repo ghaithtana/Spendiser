c9ab1b59a51c62d9098678ecc5ec84abf9759f4e
Reistana <ghaith.tana@icloud.com>
2019-04-23 13:20:46 +0300

    add user&usermanager

--- src/Controller/UserManager.java
+++ src/Controller/UserManager.java
@@ -1,5 +1,73 @@
 package Controller;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+import Model.User;
+
 public class UserManager {
+
+	public static final UserManager instance = new UserManager();
+	private User currentUser;
+
+	private UserManager() {
+
+	}
+
+	public void signUp(User u) throws Exception {
+
+		User user = getUser(u.getName());
+		if (user != null) {
+			throw new Exception("Taken username");
+		} else {
+			String sql = String.format(
+					"INSERT INTO [User] (Username,Password,Name,Gender,Birthdate) VALUES ('%s','%s','%s',%d,'%s')",
+					u.getUsername(), u.getPassword(), u.getName(), u.getGender(), u.getFormatedDate());
+
+			DatabaseManager.instance.query(sql);
+		}
+	}
+
+	public User signIn(String username, String password) throws SQLException {
+
+		String sql = String.format("EXEC [SignIn] '%s', '%s'", username, password);
+
+		ResultSet rs = DatabaseManager.instance.select(sql);
+
+		if (rs.next()) {
+			currentUser = getUserFromResultSet(rs);
+		}
+
+		return currentUser;
+	}
+
+	public User getUser(String username) throws SQLException {
+
+		String sql = String.format("SELECT * FROM [User] WHERE Username='%s'", username);
+
+		ResultSet rs = DatabaseManager.instance.select(sql);
+
+		if (rs.next()) {
+			return getUserFromResultSet(rs);
+		}
+
+		return null;
+	}
+
+	public User getCurrentUser() {
+		return currentUser;
+	}
+
+	private User getUserFromResultSet(ResultSet rs) throws SQLException {
+		int id = rs.getInt("U_ID");
+		String u = rs.getString("Username");
+		String pass = rs.getString("Password");
+		String name = rs.getString("Name");
+		Date birthdate = rs.getDate("Birthdate");
+		int gender = rs.getInt("Gender");
+
+		return new User(id, u, pass, name, gender, birthdate);
+	}
 
 }
--- src/Model/User.java
+++ src/Model/User.java
@@ -1,5 +1,72 @@
 package Model;
 
+import java.text.ParseException;
+import java.util.Date;
+
+import Controller.Utils;
+
 public class User {
 
+	private int U_ID;
+	private String username;
+	private String password;
+	private String name;
+	private int gender;
+	private Date birthdate;
+
+	public User(int ID, String username, String password, String name,
+			int gender, Date birthdate) {
+		this.U_ID = ID;
+		this.username = username.toLowerCase();
+		this.password = password;
+		this.gender = gender;
+		this.birthdate = birthdate;
+		this.name = name;
+
+	}
+
+	public User(String username, String password, String name, int gender,
+			String birthdate) throws ParseException {
+		this.username = username.toLowerCase();
+		this.password = password;
+		this.gender = gender;
+		this.name = name;
+		this.setBirthdate(birthdate);
+	}
+
+	public int getU_ID() {
+		return U_ID;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public int getGender() {
+		return gender;
+	}
+
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	public String getFormatedDate() {
+		return Utils.dateToString(birthdate);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setBirthdate(String date) {
+		this.birthdate = Utils.stringToDate(date);
+	}
 }
